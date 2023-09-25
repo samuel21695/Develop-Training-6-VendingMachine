b66f28baf9585de0b8bcc7b653186c066f92f03d
samuel21695 <samuel21695@gmail.com>
2023-09-25 16:53:48 +0900

    아이템 선택 버튼 제

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 const boxOne = document.getElementById('root').children[0]
 console.dir(boxOne)
 
+const emoticons = [
+  '😂', '😭', '😚', '😍', '😁', '🍁', '🌰', '🌲', '🌻',
+  '🌺'
+]
+
+for (i=0; i<emoticons.length; i++) {
+  const makeBtn = document.createElement('button');
+  const button = boxOne.children[i]
+  boxOne.appendChild(makeBtn);
+  boxOne.children[i].textContent = emoticons[i];
+}
+
+// const makeBtn = document.createElement('button')
+// boxOne.appendChild(makeBtn);
+btn =boxOne.children[0]
+console.dir(btn)
