38ecdecf4aaad4087d118ba9ac7663b0ef3e657b
samuel21695 <samuel21695@gmail.com>
2023-09-25 17:59:56 +0900

    아이템 중복 선택 버튼하여 backgroundColor 변경

--- script.js
+++ script.js
@@ -2,18 +2,29 @@ const boxOne = document.getElementById('root').children[0]
 console.dir(boxOne)
 
 const emoticons = [
-  '😂', '😭', '😚', '😍', '😁', '🍁', '🌰', '🌲', '🌻',
-  '🌺'
+  '😂', '😭', '😚', '😍', '😁', '🍁', '🌰', '🌲', '🌻', '🌺', '🐺', '🐗', '🐼', '🐶', '🐱', '🎤', '🎹', '🎸', '🎻', '🎷'
 ]
+const price = []
 
 for (i=0; i<emoticons.length; i++) {
   const makeBtn = document.createElement('button');
   const button = boxOne.children[i]
   boxOne.appendChild(makeBtn);
   boxOne.children[i].textContent = emoticons[i];
-}
+
+  const toggleButton = boxOne.children[i];
+  
+  toggleButton.addEventListener('click', function() {
+    if (toggleButton.style.backgroundColor === '' ) {
+      toggleButton.style.backgroundColor = 'salmon';
+    } else {
+      toggleButton.style.backgroundColor = '';
+    }
+  });
+  }
 
 // const makeBtn = document.createElement('button')
 // boxOne.appendChild(makeBtn);
-btn =boxOne.children[0]
+btn = boxOne.children[0]
 console.dir(btn)
+
