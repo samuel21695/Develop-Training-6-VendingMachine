aca622d39546c388ed6d27dfb5a9c268f001a81f
samuel21695 <samuel21695@gmail.com>
2023-09-26 16:18:25 +0900

    box 2 수정 ~ing

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
+// * box 1
 const boxOne = document.getElementById('root').children[0]
 console.dir(boxOne)
 
+// * emoji array
 const emoticons = [
-  '😂', '😭', '😚', '😍', '😁', '🍁', '🌰', '🌲', '🌻', '🌺', '🐺', '🐗', '🐼', '🐶', '🐱', '🎤', '🎹', '🎸', '🎻', '🎷'
+  '😂', '😭', '😚', '😍', '😁', '🍁', '🌰', '🌲', '🌻', '🌺', '🐺', '🐗', '🐼', '🐶', '🐱', '🎤', '🎹', '🎸', '🎻', '🎷', '⚽', '⚾', '🏀', '🏓', '🏸', '🚗', '🚌', 
+  '🚃', '🚄', '🛫'
 ]
-const price = []
 
-for (i=0; i<emoticons.length; i++) {
+// * price array (should be revised with the correct price of items.)
+const price = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13,14, 15, 16, 17 , 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+// * create emoji items && backgroundColor change
+for (i=0; i < emoticons.length; i++) {
   const makeBtn = document.createElement('button');
   const button = boxOne.children[i]
   boxOne.appendChild(makeBtn);
-  boxOne.children[i].textContent = emoticons[i];
+  boxOne.children[i].textContent = emoticons[i] + price[i];
 
   const toggleButton = boxOne.children[i];
   
@@ -25,6 +31,42 @@ for (i=0; i<emoticons.length; i++) {
 
 // const makeBtn = document.createElement('button')
 // boxOne.appendChild(makeBtn);
-btn = boxOne.children[0]
-console.dir(btn)
+// btn = boxOne.children[0]
+// console.dir(btn)
 
+// * box 2
+// * box 2 elements
+const boxtwo = document.getElementById('root').children[1];
+console.dir(boxtwo);
+
+const createDiv = document.createElement('div');
+boxtwo.appendChild(createDiv);
+
+const createInputText = document.createElement('input');
+boxtwo.appendChild(createInputText);
+
+const makeBtn = document.createElement('button');
+boxtwo.appendChild(makeBtn);
+
+
+// * text
+boxtwo.children[0].textContent = "Green Academy Vending Machine";
+
+// * input
+
+const inputT = boxtwo.children[1]
+
+// * button
+const btn = boxtwo.children[2];
+btn.textContent = "버튼"
+btn.addEventListener("click", function() {
+console.log(inputT.value)})
+
+btn.addEventListener("click", function() {
+  for (i=0; i < emoticons.length; i++) {
+    if (inputT.value = price[i]) {
+      console.log(price[i])
+    } else {
+    console.log("불일치합니다.")
+  }
+}})
