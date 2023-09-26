23f1542720109f82bcbad6cfd227412f8df25be2
samuel21695 <samuel21695@gmail.com>
2023-09-26 17:30:09 +0900

    total price setting

--- script.js
+++ script.js
@@ -12,21 +12,29 @@ const emoticons = [
 const price = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ,11, 12, 13,14, 15, 16, 17 , 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 
 // * create emoji items && backgroundColor change
+
+const initialPrice = 0
+
 for (i=0; i < emoticons.length; i++) {
   const makeBtn = document.createElement('button');
   const button = boxOne.children[i]
   boxOne.appendChild(makeBtn);
   boxOne.children[i].textContent = emoticons[i] + price[i];
-
-  const toggleButton = boxOne.children[i];
   
+  const toggleButton = boxOne.children[i];
   toggleButton.addEventListener('click', function() {
+
     if (toggleButton.style.backgroundColor === '' ) {
       toggleButton.style.backgroundColor = 'salmon';
+      // b = toggleButton.
+      totalPrice = initialPrice + i
     } else {
       toggleButton.style.backgroundColor = '';
+
     }
-  });
+    console.log(totalPrice)
+  } 
+  );
   }
 
 // const makeBtn = document.createElement('button')
@@ -59,14 +67,19 @@ const inputT = boxtwo.children[1]
 // * button
 const btn = boxtwo.children[2];
 btn.textContent = "버튼"
-btn.addEventListener("click", function() {
-console.log(inputT.value)})
+// btn.addEventListener("click", function() {
+// console.log(inputT.value)})
 
 btn.addEventListener("click", function() {
+  // console.log(a)
+  // const a = inputT.value - price[i]
+
   for (i=0; i < emoticons.length; i++) {
-    if (inputT.value = price[i]) {
-      console.log(price[i])
+    
+    if (price[i] === inputT.value) {
+      console.log("일치합니다.")
     } else {
-    console.log("불일치합니다.")
+      console.log("불일치합니다.")
+    }
   }
-}})
+})
